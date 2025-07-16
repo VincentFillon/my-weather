6ee1f704a7b6f13178246616f6cec9deb1f7b8bf
Vincent Fillon <vfillon@ics.fr>
2025-07-16 14:19:47 +0200

    Ajout d'un paramètre pour faire varier la période de récupération des stats dans mood-chart

--- src/resources/mood-chart/mood-chart.controller.ts
+++ src/resources/mood-chart/mood-chart.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query, Request, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../../guards/jwt-auth.guard'; // Assurez-vous que le chemin est correct
 import { MoodChartDataDto } from './dto/mood-chart-data.dto';
@@ -15,7 +15,8 @@ export class MoodChartController {
   @ApiOperation({ summary: "Obtenir les données du graphique d'humeur pour l'utilisateur authentifié" })
   @ApiResponse({ status: 200, description: 'Données du graphique récupérées avec succès.', type: [MoodChartDataDto] })
   @ApiResponse({ status: 401, description: 'Non autorisé.' })
-  async getMoodChartData(@Request() req): Promise<MoodChartDataDto[]> {
-    return this.moodChartService.getMoodChartData(req.user.sub);
+  async getMoodChartData(@Request() req, @Query() days: number): Promise<MoodChartDataDto[]> {
+    const nbDays = days != null ? +days : 7; // Si aucun nombre de jours n'est fourni, utiliser 7 par défaut
+    return this.moodChartService.getMoodChartData(req.user.sub, days);
   }
 }
