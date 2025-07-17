b5ea906ea64894e0a19a6499aa25106a707c871b
Vincent Fillon <vfillon@ics.fr>
2025-07-17 09:54:30 +0200

    Correctif paramètre "days" dans mood-chart-controller

--- src/resources/mood-chart/mood-chart.controller.ts
+++ src/resources/mood-chart/mood-chart.controller.ts
@@ -15,7 +15,7 @@ export class MoodChartController {
   @ApiOperation({ summary: "Obtenir les données du graphique d'humeur pour l'utilisateur authentifié" })
   @ApiResponse({ status: 200, description: 'Données du graphique récupérées avec succès.', type: [MoodChartDataDto] })
   @ApiResponse({ status: 401, description: 'Non autorisé.' })
-  async getMoodChartData(@Request() req, @Query() days: number): Promise<MoodChartDataDto[]> {
+  async getMoodChartData(@Request() req, @Query('days') days: number): Promise<MoodChartDataDto[]> {
     const nbDays = days != null ? +days : 7; // Si aucun nombre de jours n'est fourni, utiliser 7 par défaut
     return this.moodChartService.getMoodChartData(req.user.sub, days);
   }
